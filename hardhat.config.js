db8a363d772dea6fab036bbd063a24c4def7ab0f
naboeth <tien-nguyen@gmx.de>
2022-06-05 19:35:47 +0200

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+//add requirement for testing
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.7",
 };
+
+
+
