80ad48268b2b596a459b2627135ce9909207c673
naboeth <tien-nguyen@gmx.de>
2022-06-06 20:47:58 +0200

    Create certificate sol

--- contracts/MasterclassNFT.sol
+++ contracts/MasterclassNFT.sol
@@ -4,10 +4,26 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "hardhat/console.sol"; //useful for debugging
+
 contract MasterclassNFT is ERC721{ 
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    event NFTminted(address sender, uint256 tokenId);
+
     //will inherit all field and functions from ERC721
     constructor(string memory name, string memory symbol) 
         //passing to parent contract the variables from constructor
         ERC721  (name, symbol) {
+            console.log("NFT name", name);
+            console.log("with symbol:", symbol);
+            console.log("msg.sender:", msg.sender);
     }
+
+    // function mintToken(address owner)
+
+
+
 }
